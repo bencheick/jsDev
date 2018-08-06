cdd726671fed3695f3582e3523fb25a291379274
CAMARA <ben-cheick-tidiane.camara@atos.net>
2018-08-06 14:29:24 +0200

    iniate project

---
+++ learn/buildScripts/startMessage.js
@@ -0,0 +1,4 @@
+import chalk from 'chalk';
+//var chalk = require('chalk'); // pour definir la couleur de text
+
+console.log(chalk.green('Starting app in dev mode...'));//eslint-disable-line no-console
